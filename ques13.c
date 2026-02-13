5e87fcca0b1892119cc2dd5516a2ad50184d8c38
Ajitabha Tripathi <tripathi.ajitabha@gmail.com>
2026-02-13 21:07:04 +0530

    Added thirteenth DSA question

---
+++ ques13.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    int r, c;
+    int a[100][100];
+
+    // Read rows and columns
+    scanf("%d %d", &r, &c);
+
+    // Read matrix
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    int top = 0, bottom = r - 1;
+    int left = 0, right = c - 1;
+
+    while (top <= bottom && left <= right) {
+
+        // Top row
+        for (int j = left; j <= right; j++) {
+            printf("%d ", a[top][j]);
+        }
+        top++;
+
+        // Right column
+        for (int i = top; i <= bottom; i++) {
+            printf("%d ", a[i][right]);
+        }
+        right--;
+
+        // Bottom row
+        if (top <= bottom) {
+            for (int j = right; j >= left; j--) {
+                printf("%d ", a[bottom][j]);
+            }
+            bottom--;
+        }
+
+        // Left column
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                printf("%d ", a[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return 0;
+}
